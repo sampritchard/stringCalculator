264313faece0c77f7a68dc58da3a30e88a6f672e
Sam Pritchard <sampritchard@msn.com>
2017-07-31 14:55:40 +0100

    First test passed - returns 0

--- spec/CalculatorSpec.js
+++ spec/CalculatorSpec.js
@@ -1,1 +1,11 @@
+describe('Calculator', function() {
+  var calculator;
+})
 
+  beforeEach(function() {
+    calculator = new Calculator();
+  });
+
+  it('returns 0 if an empty string is paseed in', function() {
+    expect(calculator.add('')).toEqual(0);
+  });
