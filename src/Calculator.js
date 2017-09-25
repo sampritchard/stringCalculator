2389e18e4d6bc4a83ff9321bc1b99f679fe15db0
Sam Pritchard <sampritchard@msn.com>
2017-09-25 11:08:26 +0100

    Refactored code to remove regex and split different lines

--- src/Calculator.js
+++ src/Calculator.js
@@ -3,11 +3,11 @@ function Calculator() {
 }
 
 Calculator.prototype.add = function(stringNumbers) {
-  var replaceNewLines = stringNumbers.replace(/(\r\n|\n|\r)/gm,",");
-  var noComma = replaceNewLines.split(',');
+  var nonNumbers = stringNumbers.replace(/(\r\n|\n|\r)/gm,",").split(',');
+
   var sum = 0;
-  for (i = 0; i < noComma.length; i++) {
-  sum += Number(noComma[i] || 0);
+  for (i = 0; i < nonNumbers.length; i++) {
+  sum += Number(nonNumbers[i] || 0);
   };
   return sum;
 };
